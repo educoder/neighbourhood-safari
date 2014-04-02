6fe0ef5d32df104999c46a0b802d2f5109770024
Thomas Berry <thom.berry@gmail.com>
2014-04-02 15:13:46 -0400

    read page template

--- mobile/js/mobile.view.js
+++ mobile/js/mobile.view.js
@@ -253,14 +253,19 @@
 
 
   /**
-    ListView
+    Read View
   **/
   app.View.ReadView = Backbone.View.extend({
+
     template: "#notes-list-template",
+    openNoteDetailTemplate: "#open-note-detail-template",
+    planningNoteDetailTemplate: "#planning-note-detail-template",
+    cuttingNoteDetailTemplate: "#cross-note-detail-template",
+    photoNoteDetailTemplate: '#photo-note-detail-template',
 
     initialize: function () {
       var view = this;
-      console.log('Initializing ListView...', view.el);
+      console.log('Initializing ReadView...', view.el);
 
       view.collection.on('change', function(n) {
         view.render();
@@ -277,16 +282,73 @@
 
     events: {
       'click .filter-notes': 'filterNotes',
-      'click .clear-notes': 'clearNotes'
+      'click .clear-notes': 'clearNotes',
+      'click .list-item': 'showNoteDetails',
     },
 
-    filterNotes: function(){
+    filterNotes: function() {
       jQuery('.filter-notes-modal').modal('show');
     },
 
-    clearNotes: function(){
+    clearNotes: function() {
       // Trigger some sort of query
       alert('Clearing Fitlers');
+    },
+
+    showNoteDetails: function() {
+      var view = this;
+      var templateType = null;
+      var htmlContents = null;
+
+      // Setup click listener
+      jQuery('.list-item').on('click', function (event) {
+
+        // fetch model ID from DOM
+        var modelId = jQuery(event.currentTarget).data('id');
+
+        // get model from collection
+        var clickedModel = view.collection.get(modelId);
+
+
+        // set templateType and htmlContents
+
+        if (clickedModel.get('type') === 'open') {
+          templateType = view.openNoteDetailTemplate;
+          htmlContents = {
+                           'author': clickedModel.get('author'),
+                           'type': clickedModel.get('type'),
+                           'desc': clickedModel.get('body').description,
+                           'created_at': clickedModel.get('created_at')
+                          };
+        } else if (clickedModel.get('type') === 'photo_set') {
+          templateType = view.photoNoteDetailTemplate;
+          htmlContents = {
+                           'author': clickedModel.get('author'),
+                           'type': clickedModel.get('type'),
+                           'desc': clickedModel.get('body').description,
+                           'created_at': clickedModel.get('created_at')
+                          };
+        } else if (clickedModel.get('type') === 'cross_cutting') {
+          templateType = view.cuttingNoteDetailTemplate;
+          htmlContents = {
+                           'author': clickedModel.get('author'),
+                           'type': clickedModel.get('type'),
+                           'desc': clickedModel.get('body').description,
+                           'created_at': clickedModel.get('created_at')
+                          };
+        } else if (clickedModel.get('type') === 'planning') {
+          templateType = view.planningNoteDetailTemplate;
+          htmlContents = {
+                           'author': clickedModel.get('author'),
+                           'type': clickedModel.get('type'),
+                           'desc': clickedModel.get('body').description,
+                           'created_at': clickedModel.get('created_at')
+                          };
+        }
+
+        var noteDetail = _.template(jQuery(templateType).text(), htmlContents);
+        view.$el.find('.note-detail').html(noteDetail);
+      });
     },
 
     render: function () {
@@ -294,7 +356,7 @@
       console.log("Rendering ListView");
 
       // find the list where items are rendered into
-      var list = this.$el.find('ul');
+      var list = this.$el.find('.note-list');
 
       // Only want to show published notes at some point
       var publishedNotes = view.collection.where({published: true});
@@ -315,7 +377,6 @@
 
         var body = note.get('body');
         var bodyText = body[_.keys(body)[0]];
-
 
 
         var listItem = _.template(jQuery(view.template).text(), {'id': note.id, 'text': bodyText, 'me_or_others': me_or_others, 'author': note.get('author'), 'created_at': note.get('created_at')});
